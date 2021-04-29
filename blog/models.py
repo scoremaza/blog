0bfbb91f35d004a2c7138ffdc4915e911456f86f
Jonathan Constant <jconstantdevry@yahoo.com>
2021-04-29 13:58:22 -0400

    Create the QuerySet manager and Canonical

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                    self).get_queryset()\
+                         .filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -21,6 +28,8 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ('-publish',)
@@ -28,4 +37,12 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title 
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", 
+                        args=[self.publish.year,
+                              self.publish.month,
+                              self.publish.day,
+                              self.slug])
+    
 
