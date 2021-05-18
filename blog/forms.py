7e9aad004340445118d049a21547cdf68c872a89
Jonathan Constant <jconstantdevry@yahoo.com>
2021-05-18 11:59:43 -0400

    Added share and comments features

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.forms.fields import Field
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django.db.models import fields
 from .models import Comment
 
@@ -13,6 +16,19 @@ class CommentForm(forms.ModelForm):
     '''
     Form for the Comment section 
     '''
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args,**kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-group'
+        self.helper.label_class = " col-md-8"
+        self.helper.field_class = "form-control "
+        self.helper.form_method = "post"
+        self.helper.form_action = "post_detail"
+
+        self.helper.add_input(Submit('submit', 'Submit'))
     class Meta:
         model = Comment
         fields = ('name','email','body')
+    
+   
+    
